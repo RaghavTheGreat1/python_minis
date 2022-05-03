b18b1fc56e6833676bf790c58d8b46020a9e6559
Raghav <developer.raghavjoshi@gmail.com>
2022-05-04 00:40:00 +0530

    add word counter

---
+++ file_ops/word_counter.py
@@ -0,0 +1,13 @@
+file_path = 'file_ops/sample_text.txt'
+
+file = open(file= file_path)
+
+
+number_of_words = 0
+
+lines_list = file.readlines()
+
+for line in lines_list:
+    number_of_words += len(line.split(" "))
+
+print(number_of_words)
